0e6d7386e0b2ecce7c8cc7b5a3ac619cdc060331
sudo-001 <carick.atezong@facsciences-uy1.cm>
2023-08-31 22:03:19 +0100

    Adding left side bar icons

--- src/components/sections/leftSideBar/LeftSidebarLink.tsx
+++ src/components/sections/leftSideBar/LeftSidebarLink.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function LeftSidebarLink({image = "", text = ""}: Props) {
   return (
-    <div className="flex items-center gap-5 w-[500px] py-2 pl-1 cursor-pointer hover:bg-gray-500">
+    <div className="flex items-center gap-5 w-[300px] py-2 pl-1 cursor-pointer hover:bg-gray-500">
         <img className='h-[30px] w-[30px] rounded-full' src={image} alt="icon" />
         <h1 className='text-[16px] font-medium'>{text}</h1>
     </div>
--- src/components/sections/leftSideBar/index.tsx
+++ src/components/sections/leftSideBar/index.tsx
@@ -14,15 +14,15 @@ function LeftSideBar({}: Props) {
             <LeftSidebarLink image={session?.user?.image!} text={session?.user?.name!} />
 
             <LeftSidebarLink image="/icons/friend.png" text="Friends" />
-            <LeftSidebarLink image={session?.user?.image!} text="Groups" />
-            <LeftSidebarLink image={session?.user?.image!} text="Marketplace" />
-            <LeftSidebarLink image={session?.user?.image!} text="Saved" />
-            <LeftSidebarLink image={session?.user?.image!} text="Pages" />
-            <LeftSidebarLink image={session?.user?.image!} text="Events" />
-            <LeftSidebarLink image={session?.user?.image!} text="Most Recent" />
+            <LeftSidebarLink image="/icons/groups.png" text="Groups" />
+            <LeftSidebarLink image="/icons/market.png" text="Marketplace" />
+            <LeftSidebarLink image="/icons/saved.png" text="Saved" />
+            <LeftSidebarLink image="/icons/pages.png" text="Pages" />
+            <LeftSidebarLink image="/icons/events.png" text="Events" />
+            <LeftSidebarLink image="/icons/recents.png" text="Most Recent" />
 
             <div className="flex items-center gap-3 w-[200px] py-2 pl-1 cursor-pointer hover:bg-gray-300">
-                <div className="bg-gray-300 h-[30px] grid place-items-center rounded-full">
+                <div className="bg-gray-300 w-[30px] h-[30px] grid place-items-center rounded-full">
                     <BiChevronDown />
                 </div>
                 <h1 className='text-[16px] font-medium'>See More</h1>
